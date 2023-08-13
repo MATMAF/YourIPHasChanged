5f327c9cd6621588909d3901c3beaeb09a20151e
MATMAF <mathieumafille1@gmail.com>
2023-08-13 20:25:41 +0200

    Using my free API SendMail

--- YourIPHasChanged.py
+++ YourIPHasChanged.py
@@ -1,8 +1,6 @@
 import requests
 import config
 import time
-import smtplib
-from email.mime.text import MIMEText
 
 def ChangeIPInConfig(ip):
     with open('config.py', 'r') as file:
@@ -23,30 +21,9 @@ while True:
         time.sleep(config.SLEEP_TIME)
     else:
         sender_name = 'YourIPHasChanged'
-        sender_email = config.EMAIL_SENDER
         receiver_email = config.EMAIL_RECEIVER
         subject = "Your IP has changed"
         message = f"Your IP has changed from {config_ip} to {ip}\n\nPowered by @MATMAF\nhttps://www.mat.run"
-
-        smtp_server = config.SMTP_SERVER
-        smtp_port = config.SMTP_PORT
-
-        smtp_username = config.EMAIL_SENDER
-        smtp_password = config.SENDER_PASSWORD
-
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(smtp_username, smtp_password)
-
-        msg = MIMEText(message)
-        msg['From'] = f'{sender_name} <{sender_email}>'
-        msg['To'] = receiver_email
-        msg['Subject'] = subject
-
-        server.sendmail(sender_email, [receiver_email], msg.as_string())
-
-        server.quit()
-
+        SendMail = requests.get(f"https://mail.mat.run/api?name={sender_name}&receiver={receiver_email}&sub={subject}&message={message}")
         ChangeIPInConfig(ip)
-
         config_ip = ip
--- config.py
+++ config.py
@@ -1,14 +1,3 @@
-# Your IP address
 IP_ADDRESS = ""
-# Check all (sec)
+EMAIL_RECEIVER = "email@example.com"
 SLEEP_TIME = 150
-# Email sender
-EMAIL_SENDER = "sender@example.com"
-# Email receiver
-EMAIL_RECEIVER = "receiver@example.com"
-# Sender SMTP server
-SMTP_SERVER = "smtp.example.com"
-# Sender SMTP port
-SMTP_PORT = 587
-# Sender password
-SENDER_PASSWORD = "P@ssword"
